b8a4e696423b055d7497e111ea6badb24c39742d
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-02-22 20:46:11 -0600

    Fixed assertion msn in the printed string

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🎉🎉✅ Assertion Passed: 1 === 1");
+    console.log(`🎉🎉✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("😓🛑 Assertion Failed: Lighthouse Labs !== Bootcamp");
+    console.log(`😓🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
