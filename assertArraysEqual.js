706cafb315f4e1e38d5e7177b94b13e0b08f7f6c
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-02-24 12:43:41 -0600

    Adding assertArraysEqual function to lotide

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,28 @@
-const assertArraysEqual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   let test = true;
   if (array1.length !== array2.length) {
-    console.log(`😓🛑 Assertion Failed: ${array1} !== ${array2}`);
-    return;
+    test = false;
+    return test;
   }
   for (let x in array1) {
     if (array1[x] !== array2[x]) {
-      console.log(`😓🛑 Assertion Failed: ${array1} !== ${array2}`);
-      return;
+      return test = false;
     }
   }
-  console.log(`🎉🎉✅ Assertion Passed: ${array1} === ${array2}`);
-  return;
+  return test;
 };
 
-assertArraysEqual(["a", "b", "c"], ["a", "b", "c"]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([], []);
-assertArraysEqual([1.3, 0.5], [2, 0.5]);
-assertArraysEqual([0.5], [0.5, 2]);
+const assertArraysEqual = function(eqArrays) {
+  if (eqArrays) {
+    console.log(`🎉🎉✅ Assertion Passed`);
+    return;
+  }
+  console.log(`😓🛑 Assertion Failed`);
+  return;
+};
+assertArraysEqual(eqArrays(["a", "b", "c"], ["a", "b", "c"]));
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]));
+assertArraysEqual(eqArrays([], []));
+assertArraysEqual(eqArrays([1.3, 0.5], [2, 0.5]));
+assertArraysEqual(eqArrays([0.5], [0.5, 2]));
+
