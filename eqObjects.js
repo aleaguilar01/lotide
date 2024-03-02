b5b5a6d4a5068b24636eb6520380465b9fbed213
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-03-02 10:09:45 -0600

    Fixing an if or else statement

--- eqObjects.js
+++ eqObjects.js
@@ -6,19 +6,24 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let x in array1) {
+    if (array1[x] !== array2[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
   for (let key in object1) {
     if (!Object.prototype.hasOwnProperty.call(object2, key)) return false;
     if (Array.isArray(object1[key])) {
-      if (object1[key].length !== object2[key].length) {
-        return false;
-      }
-      for (let x in object1[key]) {
-        if (object1[key][x] !== object2[key][x]) {
-          return false;
-        }
-      }
+      eqArrays(object1[key], object2[key]);
     } else {
       if (object1[key] !== object2[key]) return false;
     }
