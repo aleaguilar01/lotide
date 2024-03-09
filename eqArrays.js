2246feeb31201513a392e93cf5af1de74876e374
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-03-09 17:38:15 -0600

    Implementing changes to eqArray to be able to test nested arrays. Also implementing the changes to the eqObjects, to it takes on the changes to eqArray.

--- eqArrays.js
+++ eqArrays.js
@@ -7,24 +7,22 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let test = true;
   if (array1.length !== array2.length) {
-    test = false;
-    return test;
+    return false;
   }
   for (let x in array1) {
+    if (Array.isArray(array1[x])) {
+      if (!Array.isArray(array2[x])) return false;
+      if (!eqArrays(array1[x], array2[x])) return false;
+      continue;
+    }
     if (array1[x] !== array2[x]) {
-      return test = false;
+      return false;
     }
   }
-  return test;
+  return true;
 };
 
-assertEqual(eqArrays(["a", "b", "c"], ["a", "b", "c"]), true);
-assertEqual(eqArrays(["a", "j", "c"], ["a", "b", "c"]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([1.3, 0.5], [1.3, 0.5]), true);
-assertEqual(eqArrays([1.3, 0.5], [2, 0.5]), false);
-assertEqual(eqArrays([0.5], [0.5, 2]), false);
+module.exports = eqArrays;
+
 
--- eqObjects.js
+++ eqObjects.js
@@ -1,24 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🎉🎉✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😓🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  let test = true;
-  if (array1.length !== array2.length) {
-    test = false;
-    return test;
-  }
-  for (let x in array1) {
-    if (array1[x] !== array2[x]) {
-      return test = false;
-    }
-  }
-  return test;
-};
+const eqArrays = require("./eqArrays");
 
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
