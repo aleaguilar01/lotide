9edd2f314bf41d6f73e047bfefd6ebc8d1e13603
aleaguilar01 <aleaguilar01@gmail.com>
2024-03-15 14:02:46 -0600

    addressing ai suggestions

--- test/headTest.js
+++ test/headTest.js
@@ -2,27 +2,37 @@ const assert = require('chai').assert;
 const head = require('../head');
 
 describe("Testing head", () => {
-  it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(head([1, 2, 3]), 1);
+  it("returns as number the first element of an array of numbers", () => {
+    const headElement = head([1, 2 , 3]);
+    assert.strictEqual(headElement, 1);
+    assert.typeOf(headElement, 'number');
   });
 
-  it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5');
+  it("returns as string the first number of an array of numbers as strings", () => {
+    const headElement = head(['5']);
+    assert.strictEqual(headElement, '5');
+    assert.typeOf(headElement, 'string');
   });
 
-  it('returns 5 for [5,6,7]', ()=>{
-    assert.strictEqual(head([5,6,7]), 5);
+  it('returns the first element of a mixed array', ()=>{
+    const headElement = head([5, '6', true, 5.12]);
+    assert.strictEqual(headElement, 5);
+    assert.typeOf(headElement, 'number');
   });
 
-  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', ()=>{
-    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it('returns the first string element from an array of strings', ()=>{
+    const headElement = head(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(headElement, "Hello");
+    assert.typeOf(headElement, 'string');
   });
   
-  it('returns 14.13 for [14.13,23.23,32.32]', ()=>{
-    assert.strictEqual(head([14.13,23.23,32.32]), 14.13);
+  it('returns the first element as number from an array of float numbers', ()=>{
+    const headElement = head([14.13,23.23,32.32]);
+    assert.strictEqual(headElement, 14.13);
+    assert.typeOf(headElement, 'number');
   });
 
-  it('returns "a" for ["a", "b", "c"]', ()=>{
+  it('returns as string the first element of an array of characters', ()=>{
     assert.strictEqual(head(["a", "b", "c"]), "a");
   });
 
