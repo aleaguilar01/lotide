a3d586379aab17713cc4e6523183164af662e62f
aleaguilar01 <aleaguilar01@gmail.com>
2024-03-15 13:24:31 -0600

    addressing ai comments

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -7,36 +7,36 @@ describe("Testing eqArrays", () => {
   it("should return true if the two arrays with strings are equal", () => {
     const arr1 = ["a", "b", "c"];
     const arr2 = ["a", "b", "c"];
-    assert.equal((eqArrays(arr1 , arr2)), true);
+    assert.isTrue(eqArrays(arr1 , arr2));
   });
 
   it("should return false if the two arrays with strings are different", () => {
     const arr1 = ["a", "j", "c"];
     const arr2 = ["a", "b", "c"];
-    assert.equal((eqArrays(arr1 , arr2)), false);
+    assert.isFalse(eqArrays(arr1 , arr2));
   });
 
   it("should return true if the two arrays with numbers are equal", () => {
     const arr1 = [1, 2, 3];
     const arr2 = [1, 2, 3];
-    assert.equal((eqArrays(arr1 , arr2)), true);
+    assert.isTrue(eqArrays(arr1 , arr2));
   });
 
   it("should return false if the two arrays with numbers are equal", () => {
     const arr1 = [1, 4, 3];
     const arr2 = [1, 2, 3];
-    assert.equal((eqArrays(arr1 , arr2)), false);
+    assert.isFalse(eqArrays(arr1 , arr2));
   });
 
   it("should return true if the two arrays are empty", () => {
     const arr1 = [];
     const arr2 = [];
-    assert.equal((eqArrays(arr1 , arr2)), true);
+    assert.isTrue(eqArrays(arr1 , arr2));
   });
 
   it("should return true if the two arrays with nested arrays are equal", () => {
     const arr1 = [1, [2, 3], 4, [5, [6, [7, [8]]]]];
     const arr2 = [1, [2, 3], 4, [5, [6, [7, [8]]]]];
-    assert.equal((eqArrays(arr1 , arr2)), true);
+    assert.isTrue(eqArrays(arr1 , arr2));
   });
 });
