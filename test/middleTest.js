26084ae301fc74e2e13aa937c78944cbe6f7b8f5
aleaguilar01 <aleaguilar01@gmail.com>
2024-03-15 14:32:44 -0600

    adding mocha and chai to middleTest and tailTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,34 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual((middle([1,2,3,4,5])), [3]);
-assertArraysEqual((middle([1])), []);
-assertArraysEqual((middle([1,2])), []);
-assertArraysEqual((middle([1,2,3,4,5,6,7,8,9,10])), [5,6]);
-assertArraysEqual((middle([])), []);
-assertArraysEqual((middle(["a", "e", "i"])), ["e"]);
-assertArraysEqual((middle(["a", "e", "i", "o"])), ["e", "i"]);
+describe('Testing middle', ()=>{
+
+  it('returns the middle element of an array of numbers',()=>{
+    assert.deepEqual((middle([1,2,3,4,5])), [3]);
+  });
+
+  it('returns an empty array if there original array contains just one value',()=>{
+    assert.deepEqual((middle([1])), []);
+  });
+
+  it('returns an empty array if there is no middle value',()=>{
+    assert.deepEqual((middle([1,2])), []);
+  });
+
+  it('returns two elements in the array if the array contains two middle values',()=>{
+    assert.deepEqual((middle([1,2,3,4,5,6,7,8,9,10])), [5,6]);
+  });
+
+  it('returns an empty array of an empty array',()=>{
+    assert.deepEqual((middle([])), []);
+  });
+
+  it('returns the middle character of an array',()=>{
+    assert.deepEqual((middle(["a", "e", "i"])), ["e"]);
+  });
+
+  it('returns the two middle characters of an array with two middle values',()=>{
+    assert.deepEqual((middle(["a", "e", "i", "o"])), ["e", "i"]);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,20 +1,35 @@
-
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const result = tail([1, 2, 3, 4]);
-assertEqual(result.length, 3);
-assertEqual(result[0], 2);
-assertEqual(result[1], 3);
-assertEqual(result[2], 4);
 
-const result2 = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result2.length, 2);
-assertEqual(result2[0], "Lighthouse");
-assertEqual(result2[1], "Labs");
+describe('Testing middle', ()=>{
+  it('should return an array without the first element of an array of numbers', ()=>{
+    const input = [1, 2, 3, 4];
+    const expectedOutput = [2, 3, 4];
+    const result = tail(input);
+    assert.equal(result.length, 3);
+    assert.deepEqual(tail(input), expectedOutput);
+  });
+  
+  it('should return an array without the first element of an array of strings', ()=>{
+    const input = ["Hello", "Lighthouse", "Labs"];
+    const expectedOutput = ["Lighthouse", "Labs"];
+    const result = tail(input);
+    assert.equal(result.length, 2);
+    assert.deepEqual(tail(input), expectedOutput);
 
-const result3 = tail(["onlyElement"]);
-assertEqual(result3.length, 0);
+  });
+  
+  it('should return an empty array when input array has only one element', ()=>{
+    const result = tail(["onlyElement"]);
+    assert.equal(result.length, 0);
+  });
+  
+  it('should return an empty array when input array is empty', ()=>{
+    const result = tail([]);
+    assert.equal(result.length, 0);
+  });
 
-const result4 = tail([]);
-assertEqual(result4.length, 0);
+});
+
+
